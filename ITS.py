9f0fcfd189bd4eb1613743e324c01a3caf4cf798
Freakhuan <freakhuan@gmail.com>
2017-06-26 22:56:37 +0900

    flag for already booked

--- ITS.py
+++ ITS.py
@@ -3,28 +3,31 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver.support.ui import Select
 
-url1="https://as.its-kenpo.or.jp/apply/empty_new?s=PVVqTTIwRFpwWlNaMUpIZDlrSGR3MVda"
-url2="https://as.its-kenpo.or.jp/apply/empty_new?s=PVlqTTIwRFpwWlNaMUpIZDlrSGR3MVda"
-dates_in_jpy=["2017年05月03日","2017年05月04日","2017年05月05日","2017年05月06日"]
-dates_in_eng=["2017-05-03","2017-05-04","2017-05-05","2017-05-06"]
-email="********@gmail.com"
+url1="https://as.its-kenpo.or.jp/apply/empty_new?s=PUFETzIwRFpwWlNaMUpIZDlrSGR3MVda"
+url2="https://as.its-kenpo.or.jp/apply/empty_new?s=PWd6TjIwRFpwWlNaMUpIZDlrSGR3MVda"
+url3="https://as.its-kenpo.or.jp/apply/empty_new?s=PWNqTjIwRFpwWlNaMUpIZDlrSGR3MVda"
+dates_in_jpy=["2017年07月07日","2017年07月08日"]
+dates_in_eng=["2017-07-07","2017-07-08"]
+urls=[url1,url2,url3]
+email="**********@gmail.com"
+flags=[[0 for i in range(len(dates_in_jpy))] for i in range(len(urls))]
+result=-1
 
 browser=webdriver.Chrome()
 
 while 1:
  try:
-  for num in range(len(dates_in_jpy)):
-   browser.get(url1)
-   dates=browser.find_element_by_id("apply_join_time").text.encode('utf-8')
-   result=dates.find(dates_in_jpy[num])
-   if result == -1:
-    browser.get(url2)
-    dates=browser.find_element_by_id("apply_join_time").text.encode('utf-8')
-    result=dates.find(dates_in_jpy[num])
-   if result != -1:
-    index=num
-    break
-   
+  for i in range(len(urls)):
+   for j in range (len(dates_in_jpy)):
+    if flags[i][j] != 1:
+     browser.get(urls[i])
+     dates=browser.find_element_by_id("apply_join_time").text.encode('utf-8')
+     result=dates.find(dates_in_jpy[j])
+    if result != -1:
+     flags[i][j]=1
+     index=j
+     break
+
   if result != -1:
    print "Greate!!", dates_in_jpy[index]
    dropdown=Select(browser.find_element_by_id("apply_join_time"))
